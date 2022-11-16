b194e2317a44203759a97198bd37fce6bc9d93cb
SpaceMarco_WindowsPC <black1cl0ud99@gmail.com>
2022-11-16 22:43:55 +0200

    fixed url in FE enviroment

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://p3-api-dev.eba-7bhmmmmh.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
