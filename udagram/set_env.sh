07b6cc8a5868c5fb8679e0c71c13abba84af05a4
SpaceMarco_WindowsPC <black1cl0ud99@gmail.com>
2022-11-19 19:38:05 +0200

    circleCI configured

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,16 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST="database-1.cmu9rx1ywe2s.us-east-1.rds.amazonaws.com"
+export POSTGRES_USERNAME =postgres
+export POSTGRES_PORT=5432
+export PORT=8080
+export POSTGRES_PASSWORD=postgres
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export AWS_REGION="us-east-1"
+export AWS_PROFILE="default"
+export AWS_BUCKET="arn:aws:s3:::899113371307-bucket-udagram"
+export AWS_ACCESS_KEY_ID="AKIA5CV2VI2V5NCAVOUS"
+export AWS_SECRET_ACCESS_KEY="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"
+export AWS_S3_ENDPOINT="http://899113371307-bucket-udagram.s3-website-us-east-1.amazonaws.com/"
+export URL="http://899113371307-bucket-udagram.s3-website-us-east-1.amazonaws.com/"
+export JWT_SECRET="123ABC"
